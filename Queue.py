d3521bf27eba7be1628f522c083cdeedbdf6cc77
mcgovs5 <146123846+mcgovs5@users.noreply.github.com>
2023-11-30 19:06:04 -0500

    Add files via upload

---
+++ Queue.py
@@ -0,0 +1,15 @@
+names = []
+
+nameCount = 0
+for i in range(10):
+    name = input("Enter name: ")
+    names.append(name)
+    nameCount +=1
+
+
+print(names)
+
+for i in range(10):
+    len(names)
+    print(names.pop(0))
+    
